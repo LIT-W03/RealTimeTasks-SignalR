c2cd6777a7a0ba6426e6b9e503374793b572a693
Avrumi Friedman <a.friedman07@gmail.com>
2017-06-07 22:14:51 -0400

    Initial commit

---
+++ RealTimeTasks.Web/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using RealTimeTasks.Data;
+using RealTimeTasks.Web.Models;
+
+namespace RealTimeTasks.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            var repo = new UserRepository(Properties.Settings.Default.ConStr);
+            var user = repo.GetByEmail(User.Identity.Name);
+            return View(new IndexViewModel { UserId = user.Id });
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            var repo = new UserRepository(Properties.Settings.Default.ConStr);
+            var user = repo.Login(email, password);
+            if (user == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            FormsAuthentication.SetAuthCookie(user.EmailAddress, false);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user, string password)
+        {
+            var repo = new UserRepository(Properties.Settings.Default.ConStr);
+            repo.AddUser(user, password);
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
